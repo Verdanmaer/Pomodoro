693c408dd0aae8c96145b6dbc12c83446f77a62c
Adam Verner <verneradam618@gmail.com>
2019-02-12 17:32:20 +0100

    Updates

---
+++ precache-manifest.7d47b1077600d4d7a5713a25280c9ea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1852307fcea59e4e33ec4e86f0a7bee8",
+    "url": "/projects/Pomodoro/static/media/clock.1852307f.jpg"
+  },
+  {
+    "revision": "dcc37a2fb03efa21ca5a13772ebe6f07",
+    "url": "/projects/Pomodoro/static/media/beep.dcc37a2f.wav"
+  },
+  {
+    "revision": "21226ddd4c63edbafeef",
+    "url": "/projects/Pomodoro/static/js/runtime~main.21226ddd.js"
+  },
+  {
+    "revision": "2d13c0ab46280c8489c9",
+    "url": "/projects/Pomodoro/static/js/main.2d13c0ab.chunk.js"
+  },
+  {
+    "revision": "af94c3f4985434b9a610",
+    "url": "/projects/Pomodoro/static/js/1.af94c3f4.chunk.js"
+  },
+  {
+    "revision": "2d13c0ab46280c8489c9",
+    "url": "/projects/Pomodoro/static/css/main.1c62270b.chunk.css"
+  },
+  {
+    "revision": "8be70e5357b53543ee66ddcfcaf06f7e",
+    "url": "/projects/Pomodoro/index.html"
+  }
+];
